0d4f305d2c5885b0efbb028dc7b4c696fa07fbf3
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2021-12-22 23:31:21 +0200

    added trailer

--- src/js/components/plug.js
+++ src/js/components/plug.js
@@ -1,7 +1,13 @@
 import plug from '../../images/content/plug.png';
+import youtubeError from '../../images/content/youtube-broken.jpg';
 import refs from '../refs';
 
 export function plugMarkup() {
   const markup = `<div class="plug-box"><img src="./${plug}" /></div>`;
   refs.contentRef.insertAdjacentHTML('beforeend', markup);
+}
+
+export function trailerErrorMarkup() {
+  const markup = `<div class="error-box"><img class="error-box__img" width="1000" height="600" src="./${youtubeError}" alt="trailer not found" class="trailer"></div>`;
+  refs.modalRef.innerHTML = markup;
 }
